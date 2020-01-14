f7b73f145e259e7910f9793b1c0949706b4d3308
Nagaraj shetty <nagarajmanjunath22@gmail.com>
2020-01-14 10:37:35 +0800

    changes made in web3

--- calculator/build.sh
+++ calculator/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cargo build --release --target wasm32-unknown-unknown
-wasm-build --target=wasm32-unknown-unknown ./target pwasm_tutorial_contract
+wasm-build --target=wasm32-unknown-unknown ./target calculator
 
